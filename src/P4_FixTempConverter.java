d9932878108c7b3e96a876f3d47dd298423d2f08
jmorgan20 <37450743+jmorgan20@users.noreply.github.com>
2018-04-22 14:39:42 -0500

    Completed

--- src/P4_FixTempConverter.java
+++ src/P4_FixTempConverter.java
@@ -6,20 +6,20 @@
 
 /*
  What were the errors you found?
- 
- 
+ Line 17  needed to be changed to double answer and on line 18 answer needed to be entered to correct the results. also had to change the return on the last line to get the correct answer. 
+ line 22 had some edits also. 
  */
 
 public class P4_FixTempConverter {
 	public static void main(String[] args) {
 		double tempf = 98.6;
 		double tempc = 0.0;
-		ftoc(tempf, tempc);
-		System.out.println("Body temp in C is: " + tempc);
+		double answer = ftoc(tempf);
+		System.out.println("Body temp in C is: " + answer);
 	}
 
 	// converts Fahrenheit temperatures to Celsius
-	public static void ftoc(double tempf, double tempc) {
-	      tempc = (tempf - 32) * 5 / 9;
+	public static double ftoc(double tempf1) {
+	      return (tempf1 - 32) * 5 / 9;
     }
 }
--- src/P5_TestMathFunctionCalls.java
+++ src/P5_TestMathFunctionCalls.java
@@ -7,21 +7,21 @@
 /*
  5. Your predicted expression values:
  
- 	1.
- 	2.
- 	3.
- 	4.
- 	5.
- 	6.
- 	7.
- 	8.
+ 	1.1.6
+ 	2.6,2 answer 36
+ 	3.7 max is 7
+ 	4.-2,-5 answer was -5 for min
+ 	5.6,5 answer was just 5
+ 	6.squareroot is 8
+ 	7.17
+ 	8.14 is the correct answer.
   
   
  */
 public class P5_TestMathFunctionCalls {
 
 	public static void main(String[] args) {
-		System.out.println("REPLACE WITH YOUR EXPRESSION");
+		System.out.println(Math.max( 18 - 5, Math.ceil( 4.6 * 3 )));
 	}
 
 }
